c6602841bc4d5197889ae1aa6ae557949670d69f
Said-Elabidy <saidelabidy131@gmail.com>
2025-06-01 11:05:39 +0300

    Solutions of  2 Problems on LeetCode

---
+++ LeetCode/Array&String/Remove Element/Remove Element/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Element
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    counter++;
+                }
+
+            }
+
+            int[] ints =new int[counter] ;
+            int c = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                
+                if (nums[i] != val)
+                {
+                    ints[c] = nums[i];
+                    c++;
+                }
+
+            }
+            ints.CopyTo(nums,0);
+
+            return ints.Length;
+
+
+        }
+
+    }
+
+}

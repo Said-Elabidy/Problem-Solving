b9518966a2af9a1486403b870158e5c1233dab2b
Said-Elabidy <saidelabidy131@gmail.com>
2025-06-09 09:39:50 +0300

    Add Problem 3

---
+++ LeetCode/Array&String/Remove-Duplicates-From-Sorted-Array/Remove-Duplicates-From-Sorted-Array/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Remove_Duplicates_From_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 1, 2, 2, 3, 4 };
+
+            Solution solution = new Solution();
+
+            int newLength = solution.RemoveDuplicates(ints);
+
+            Console.WriteLine("New length: " + newLength);
+
+            Console.Write($"Modified array: ");
+            foreach (var item in ints)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ LeetCode/Array&String/Remove-Duplicates-From-Sorted-Array/Remove-Duplicates-From-Sorted-Array/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Remove_Duplicates_From_Sorted_Array
+{
+    internal class Solution
+    {// [1 ,1 , 2, 2, 3, 4] -> [1, 2, 3, 4]
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int i = 0;
+
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+
+    }
+}
